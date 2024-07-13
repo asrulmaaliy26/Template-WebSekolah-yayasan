e05118074cbed8f3fcbda6f8e6c48d218ea7f8e8
asrulmaaliy26 <lostsaga06k@gmail.com>
2024-07-13 18:46:39 +0700

    first commit

---
+++ akreditasi.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SMK Telkom Purwokerto</title>
+  
+ 
+  <?php include 'snippets/styles.php' ?>
+</head>
+<body>
+
+    
+  <?php include 'snippets/navbar.php' ?>
+
+  <section>
+  <img style="margin-top: 60px;" width="100%" src="assets/img/header/akreditasi.png" alt="">
+  </section>
+
+  <!-- section -->
+  <div class="container pt-5 pb-5 ak1">
+      <div class="row text-center pt-5">
+        <h4 class="pb-5">Pernyataan Bahwa SMK Telkom Purwokerto<br>
+          Terakreditasi A</h4>
+          <div class="col-md-6 pt-2 text-center">
+            <img style="width: 90%;" src="assets/img/akreditasi/akr1.jpg" alt="">
+          </div>
+          <div class="col-md-6 pt-2">
+            <img style="width: 90%;" src="assets/img/akreditasi/akr2.jpg" alt="">
+          </div>
+      </div>
+      <div class="row text-center pt-5">
+        <a href="assets/img/Akreditasi-Sekolah-2019-2023-SMK-Telekomukinasi-Sandhy-Putra-Purwokerto.pdf"> <button style="background-color: #9B1E01; border:none; width: 200px;" type="button" class="btn btn-danger justify-content-center mt-4 mb-4">Lihat PDF Disini</button></a>
+      </div>
+  </div>
+  <!-- akhir section -->
+
+  <?php include 'snippets/footer.php' ?>
+  
+  <?php include 'snippets/scripts.php' ?>
+
+</body>
+</html>
---
+++ struktur_organisasi.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SMK Telkom Purwokerto</title>
+
+  <?php include 'snippets/styles.php' ?>
+</head>
+<body>
+
+       <!-- NAVBAR -->
+  <?php include 'snippets/navbar.php' ?>
+      <!-- AKHIR NAVBAR -->
+
+  <section>
+    <img style="width: 100%; margin-top: 60px;" src="assets/img/header/struktur.png" alt="">
+  </section>
+
+    
+  <section>
+    <div class="container pt-5 pb-5">
+        <img style="width:100%; padding: 100px;" src="assets/img/struktur_organisasi.jpg" alt="">
+    </div>
+  </section>
+
+
+  <?php include 'snippets/footer.php' ?>
+  <?php include 'snippets/scripts.php' ?>
+</body>
+</html>
